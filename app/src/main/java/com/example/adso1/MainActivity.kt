9abb4d3fa809b9d50d0df5e62feeb1c133cf7efe
Alexgavi11 <alexgabo1108@gmail.com>
2024-09-01 21:32:11 -0500

    primer modulo

---
+++ app/src/main/java/com/example/adso1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.adso1
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+    }
+    fun MetodoBoton(view: View) {
+        val editText= findViewById<EditText>(R.id.editText)
+        val nombre= editText.text.toString()
+        Toast.makeText(this, "Bienvenido $nombre", Toast.LENGTH_SHORT).show()
+
+    }
+}
